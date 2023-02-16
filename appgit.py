7e09b23250b0911abf396b8efdd6827c3a8ad02a
diogodreyfuss <didioguinhods@gmail.com>
2023-02-16 17:34:29 -0300

    ok

--- appgit.py
+++ appgit.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
 st.write("# Novo Aplicativo")
 
 
 l = np.random.randint(5,20,10)
 st.write(l)
 
-plt.plot(np.random.randn(200))
+
+
